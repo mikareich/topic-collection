80a752ae3ddaee812388291c4ef0da2fd51d854f
Mika Reich <mikareich0@gmail.com>
2021-10-14 14:48:27 +0200

    encode heading ids

--- components/ContentRenderer.tsx
+++ components/ContentRenderer.tsx
@@ -12,8 +12,49 @@ interface ContentRendererProps {
 }
 
 function ContentRenderer({ content }: ContentRendererProps) {
+  interface HeadingProps {
+    children: React.ReactNode | React.ReactNode[];
+    id: string;
+  }
+
   const components: Partial<NormalComponents & SpecialComponents> = {
     a: ({ children, href }) => <Link href={href!}>{children}</Link>,
+    // @ts-ignore
+    h1: ({ children, id, ...props }: HeadingProps) => (
+      <h1 id={encodeURI(id)} {...props}>
+        {children}
+      </h1>
+    ),
+    // @ts-ignore
+    h2: ({ children, id, ...props }: HeadingProps) => (
+      <h2 id={encodeURI(id)} {...props}>
+        {children}
+      </h2>
+    ),
+    // @ts-ignore
+    h3: ({ children, id, ...props }: HeadingProps) => (
+      <h3 id={encodeURI(id)} {...props}>
+        {children}
+      </h3>
+    ),
+    // @ts-ignore
+    h4: ({ children, id, ...props }: HeadingProps) => (
+      <h4 id={encodeURI(id)} {...props}>
+        {children}
+      </h4>
+    ),
+    // @ts-ignore
+    h5: ({ children, id, ...props }: HeadingProps) => (
+      <h5 id={encodeURI(id)} {...props}>
+        {children}
+      </h5>
+    ),
+    // @ts-ignore
+    h6: ({ children, id, ...props }: HeadingProps) => (
+      <h6 id={encodeURI(id)} {...props}>
+        {children}
+      </h6>
+    ),
   };
 
   return (
--- utils/generateToc.ts
+++ utils/generateToc.ts
@@ -1,3 +1,4 @@
+import GithubSlugger from "github-slugger";
 // @ts-ignore
 import mdToc from "markdown-toc-unlazy";
 
@@ -10,14 +11,22 @@ interface Toc {
 interface mdTocProps {
   lvl: number;
   content: string;
-  slug: string;
 }
 
+const slugger = new GithubSlugger();
+
+/**
+ * Creates a table of contents from a markdown file.
+ * @param md Markdown content
+ * @returns Table of contents
+ */
 function generateToc(md: string): Array<Toc> {
-  const parseToc = ({ lvl, content, slug }: mdTocProps): Toc => ({
+  slugger.reset();
+
+  const parseToc = ({ lvl, content }: mdTocProps): Toc => ({
     level: lvl,
     title: content,
-    slug,
+    slug: encodeURI(slugger.slug(content)),
   });
 
   const toc = mdToc(md).json.map(parseToc);
