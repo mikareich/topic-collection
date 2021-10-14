958e5f36d658724335c5a2830f3cd67e859421f1
Mika Reich <mikareich0@gmail.com>
2021-10-14 13:37:50 +0200

    remove unused and add active prop

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,44 +1,23 @@
-/* import NextLink from "next/link";
-import React, { ReactFragment } from "react";
-
-interface LinkProps {
-  href: string;
-  icon?: ReactFragment | undefined;
-}
-
-function Link({ href, icon, children }: React.PropsWithChildren<LinkProps>) {
-  return (
-    <NextLink passHref href={href}>
-     
-      <a
-        href="#"
-        className="text-gray-500 hover:text-blue-500 rounded transition flex items-center"
-      >
-        {icon && <div className="mr-1">{icon}</div>}
-        <div className="truncate flex-1">{children}</div>
-      </a>
-    </NextLink>
-  );
-}
-
-export default Link;
- */
-
 import NextLink from "next/link";
-import React, { ReactFragment } from "react";
+import React from "react";
 
 interface LinkProps {
   href: string;
-  icon?: ReactFragment | undefined;
+  active?: boolean;
 }
 
-function Link({ href, icon, children }: React.PropsWithChildren<LinkProps>) {
+function Link({
+  href,
+  active = false,
+  children,
+}: React.PropsWithChildren<LinkProps>) {
   return (
     <NextLink passHref href={href}>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a
         href="#"
-        className="text-gray-500 hover:text-blue-500 rounded transition"
+        className={`text-gray-500 hover:text-blue-500 transition 
+                    ${active && "text-blue-500"}`}
       >
         {children}
       </a>
